a987a786dabf395c4a5f1486bd5392a8f81ff0a5
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2023-07-05 11:27:52 +0800

    Update vps.py

--- PythonCode/vps.py
+++ PythonCode/vps.py
@@ -26,9 +26,9 @@ with open(dirs+'/vps.txt', 'w+', encoding='utf-8') as f:
     # print(data)
     info ='# '+time+'更新 \n' + '# 本txt文件由Actions定时生成\n# 用于VPS国际、国内分流\n# 项目地址：https://github.com/xhrzg2017/AdGuard-Home\n'
     f.write(info)
-    outsea = 'https://dns11.quad9.net/dns-query\nhttps://doh.opendns.com/dns-query\n'
+    outsea = 'https://dns11.quad9.net/dns-query\nhttps://dns.google/dns-query\n'
     f.write(outsea)
-    dns = 'https://dns.google/resolve\n'
+    dns = 'https://doh.opendns.com/dns-query\n'
     f.write(dns)
     outside = '[/webstatic.hoyoverse.com/]'+dns
     f.write(outside)
