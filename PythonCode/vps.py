8a7e1a8dab32c8b5883e0ae2bb6ee37986baa9f2
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2023-08-18 08:23:31 +0000

    Update vps.py

--- PythonCode/vps.py
+++ PythonCode/vps.py
@@ -10,28 +10,28 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 }
 #3.构建请求对象
-request = urllib.request.Request(url, headers=headers)  #使用Request可以加请求头对象
+request = urllib.request。Request(url, headers=headers)  #使用Request可以加请求头对象
 #4.发送请求对象
-response = urllib.request.urlopen(request,context=context)
+response = urllib.request。urlopen(request,context=context)
 #5.读取数据
 data = response.read()
 #print(data)
 #6.保存到文件中 验证数据
 dirs = './'
-if not os.path.exists(dirs):
+if not os.path。exists(dirs):
     os.makedirs(dirs)
-utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
-time = utc_dt.astimezone(timezone(timedelta(hours=8))).strftime('%Y-%m-%d %H:%M')
-with open(dirs+'/vps.txt', 'w+', encoding='utf-8') as f:
+utc_dt = datetime.utcnow()。replace(tzinfo=timezone.utc)
+time = utc_dt.astimezone(timezone(timedelta(hours=8)))。strftime('%Y-%m-%d %H:%M')
+with open(dirs+'/vps.txt'， 'w+', encoding='utf-8') as f:
     # print(data)
     info ='# '+time+'更新 \n' + '# 本txt文件由Actions定时生成\n# 用于VPS国际、国内分流\n# 项目地址：https://github.com/xhrzg2017/AdGuard-Home\n'
     f.write(info)
-    outsea = 'https://dns11.quad9.net/dns-query\nhttps://dns.google/dns-query\n'
+    outsea = '168.126.63.1\n168.95.192.1\n203.112.2.4\n'
     f.write(outsea)
-    dns = 'https://doh.opendns.com/dns-query\n'
+    dns = '218.102.23.228\n'
     f.write(dns)
     outside = '[/webstatic.hoyoverse.com/]'+dns
     f.write(outside)
     f.close()
-with open(dirs+'/vps.txt', 'ab') as f:
+with open(dirs+'/vps.txt'， 'ab') as f:
     f.write(data)
